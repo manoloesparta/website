951851c13168ea975a359f281b765bf3036a50ab
manolo esparza <manoloesparta@gmail.com>
2020-02-10 12:07:36 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import "./sass/styles.scss"
+import "bootstrap/dist/js/bootstrap.min.js";
+import './sass/styles.scss';
+
+import Vue from 'vue/dist/vue.min.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const path = require("path")
 module.exports = {
   entry: {
     main: "./src/index.js",
-    vendor: "./src/vendor.js"
   },
   mode: "development",
   output: {
