109e8eeff0892721b669f3311d44d2913bee112a
manoloesparta <manoloesparta@gmail.com>
2021-01-10 15:58:48 -0800

    favicon and body padding added

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const chosen = randomNumberRange(128512, 128567)
 const emoji = document.getElementById('emoji')
 emoji.innerHTML = `&#${chosen};`
 
-// Add that emoji to the title
-const title = document.getElementsByTagName('title')[0]
-title.innerHTML = `&#${chosen}; ` + title.innerHTML
-
 // Change to Manolo Esparta
 emoji.addEventListener('click', () => {
     const header = document.getElementById('header')
