bffb9745a9facd5a9906084f8dd82c42b36de565
manoloesparta <manoloesparta@gmail.com>
2021-01-21 21:23:31 -0800

    webpack added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+// Imports
+import './styles.css'
+
 // Random numbers lesgooo
-randomNumberRange = (min, max) => Math.floor(Math.random() * (max - min) + min)
+const randomNumberRange = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
 // Generate a random emoji
 const chosen = randomNumberRange(128512, 128567)
@@ -8,52 +11,52 @@ emoji.innerHTML = `&#${chosen};`
 
 // Change to Manolo Esparta
 emoji.addEventListener('click', () => {
-    const header = document.getElementById('header')
-    header.innerHTML = 'Manolo Esparta'
+  const header = document.getElementById('header')
+  header.innerHTML = 'Manolo Esparta'
 })
 
 // Social button
 const github = document.getElementById('github')
 github.addEventListener('click', () => {
-    window.open('https://github.com/manoloesparta')
+  window.open('https://github.com/manoloesparta')
 })
 
 const twitter = document.getElementById('twitter')
 twitter.addEventListener('click', () => {
-    window.open('https://twitter.com/manoloesparta')
+  window.open('https://twitter.com/manoloesparta')
 })
 
 const discord = document.getElementById('discord')
 discord.addEventListener('click', () => {
-    if(discord.innerHTML == 'Discord') {
-        discord.innerHTML = 'manoloesparta#2766'
-    } else {
-        discord.innerHTML = 'Discord'
-    }
+  if(discord.innerHTML == 'Discord') {
+    discord.innerHTML = 'manoloesparta#2766'
+  } else {
+    discord.innerHTML = 'Discord'
+  }
 })
 
 const linkedin = document.getElementById('linkedin')
 linkedin.addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/manuel-esparza-guzman-9b6948165/')
+  window.open('https://www.linkedin.com/in/manuel-esparza-guzman-9b6948165/')
 })
 
 // Projects
 const pi = document.getElementById('pi')
 pi.addEventListener('click', () => {
-    window.open('https://github.com/manoloesparta/pi')
+  window.open('https://github.com/manoloesparta/pi')
 })
 
 const wiki = document.getElementById('wiki')
 wiki.addEventListener('click', () => {
-    window.open('https://github.com/manoloesparta/wikigraph')
+  window.open('https://github.com/manoloesparta/wikigraph')
 })
 
 const hn = document.getElementById('hn')
 hn.addEventListener('click', () => {
-    window.open('https://github.com/manoloesparta/hnjournal')
+  window.open('https://github.com/manoloesparta/hnjournal')
 })
 
 const guitar = document.getElementById('guitar')
 guitar.addEventListener('click', () => {
-    window.open('https://github.com/manoloesparta/angryguitar')
+  window.open('https://github.com/manoloesparta/angryguitar')
 })
