5173df26eeecb545f28a0a52c5200ba9cdd4838d
Manuel Esparza <manoloesparta@gmail.com>
2025-04-23 13:32:12 -0600

    squash

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import Education from "./components/Education";
-import Experience from "./components/Experience";
 import WhoAmI from "./components/Whoami";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 
 function App() {
   return (
-    <div class="max-w-screen-lg container mx-auto">
-      <Header />
+    <div className="max-w-screen-lg container mx-auto">
       <WhoAmI />
-      <Experience />
-      <Education />
-      <Projects />
-      <Footer />
     </div>
   );
 }
--- src/components/Whoami.js
+++ src/components/Whoami.js
@@ -1,45 +1,55 @@
 import React from "react";
-import me from "../assets/me.jpg";
+import me from "../assets/me2.jpg";
 import { Hr } from "./utils";
 
 export default function WhoAmI() {
   const openLinkedin = () =>
     window.open("https://www.linkedin.com/in/manuel-esparza-9b6948165/");
   const openGithub = () => window.open("https://github.com/manoloesparta");
+  const openResume = () => window.open("./CV_MANUEL_ESPARZA_GUZMAN.pdf");
+  const openTwitter = () => window.open("https://x.com/slopydev");
+  const openBlog = () => window.open("https://blog.mano.lol");
 
   return (
     <div className="p-5 pb-0">
-      <h2 className="text-2xl font-semibold">Who am I?</h2>
+      <h2 className="text-2xl font-semibold">Manuel Esparza Guzman</h2>
       <Hr />
-      <div className="xl:flex lg:flex md:flex">
-        <div className="pic wrapper block" style={{ maxWidth: 250 }}>
-          <img className="picture" src={me} alt="this is me" />
+      <div className="text-center">
+        <div
+          className="pic wrapper block object-contain"
+          style={{ maxWidth: 250 }}
+        >
+          <img className="picture object-cover" src={me} alt="this is me" />
         </div>
 
-        <div className="flex-2 sm:flex-none ml-5 mt-3">
-          <p className="mb-2">
-            <span className="font-semibold">Languages: </span>Java, Python, Go,
-            Typescript, C/C++
-          </p>
-          <p className="mb-2">
-            <span className="font-semibold">Cloud: </span>AWS, Linux, Docker,
-            OCI, Terraform, Kubernetes
-          </p>
-          <p className="mb-2">
-            <span className="font-semibold">Backend: </span>Flask, Django,
-            Vertx, Express, FastAPI
-          </p>
-          <p>
-            <span className="font-semibold">Frontend: </span>React & Redux,
-            Redwood, Tailwind
+        <div className="flex-2 sm:flex-none overflow-hidden">
+          <p className="mb-2 text-wrap text-justify">
+            I'm a software engineer based in Guadalajara, Mexico, with
+            experience working across backend, frontend, and cloud
+            infrastructure. I studied Computer Science at CETYS Universidad,
+            where I focused on AI, and since then I've been building scalable
+            systems and tools in real-world environments. Right now, I work at
+            Oracle, where I'm involved in a little bit of everything—from
+            backend development in Java, a lightweight agent for compute
+            instance in Go, operate and enhance a Kubernetes cluter, to
+            creating production-ready React apps based on Figma designs.
           </p>
 
-          <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 mt-3">
+          <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-4 grid-cols-3 mt-3">
+            <button onClick={openResume} className="block social">
+              Resume
+            </button>
             <button onClick={openGithub} className="block social">
               Github
             </button>
             <button onClick={openLinkedin} className="block social">
               Linkedin
+            </button>
+            <button onClick={openTwitter} className="block social">
+              Twitter
+            </button>
+            <button onClick={openBlog} className="block social">
+              Blog
             </button>
           </div>
         </div>
